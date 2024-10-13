138aabd0e9f86e253ce0a1e57abc5af1f22fe3fa
Anna Starkova <anutashamonina@yandex.ru>
2024-10-13 22:51:04 +0300

    На проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardParams, deleteFunction){
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  let deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteFunction);
+  deleteButton.targetElement = cardElement;
+  cardElement.querySelector('.card__title').textContent = cardParams.name;
+  cardElement.querySelector('.card__image').setAttribute('src', cardParams.link);
 
+  return cardElement;
+  // placesList.append(cardElement);
+}
 // @todo: Функция удаления карточки
+function deleteCard(event){
+  event.currentTarget.targetElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardParams) => {
+  card = createCard(cardParams, deleteCard);
+  placesList.append(card);
+});
+
